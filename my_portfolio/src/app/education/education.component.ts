5cc908bf1de00404ee34cb6547d55be9d49f9e8a
Anukrit Tiwari <anukrit01@gmail.com>
2023-04-28 10:55:46 +0530

    personal-info and education module added

--- my_portfolio/src/app/education/education.component.ts
+++ my_portfolio/src/app/education/education.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Education } from '../models/models';
 
 @Component({
   selector: 'app-education',
@@ -6,6 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./education.component.scss']
 })
 export class EducationComponent implements OnInit {
+  educationList: Education[] = [
+    {
+      institute: 'National Institute of Technology, Hamirpur',
+      course: 'MTech',
+      duration: '2019-2021',
+      score: '8.87'
+    },
+    {
+      institute: 'Rewa Institute of Technology, Rewa',
+      course: 'BE',
+      duration: '2014-2018',
+      score: '7.61'
+    },
+    {
+      institute: 'Jawahar Navodaya Vidyalaya Sirmour',
+      course: '12th',
+      duration: '2013-2014',
+      score: '65%'
+    }
+  ];
 
   constructor() { }
 
--- my_portfolio/src/app/personal-information/personal-information.component.ts
+++ my_portfolio/src/app/personal-information/personal-information.component.ts
@@ -6,6 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./personal-information.component.scss']
 })
 export class PersonalInformationComponent implements OnInit {
+  myData: string[][] = [
+    ['Name', 'Anukrit Tiwari'],
+    ['DOB', '14/10/1997'],
+    ['Work Exp', '1.6 Years'],
+    ['Education', 'MTech (2021)'],
+    ['Interests', 'Coding']
+  ];
+
+  aboutMe: string[] = [
+    ' Hi, I am an experienced Software Engineer with demonstrated history of working in IT industry using Agile model.',
+    'Skilled in JavaScript, TypeScript, C++, Python, Data Structures and Algorithms.',
+    'Strong engineering professional with a Master of Technology - MTech focused in Computer Science & Engineering from National Institute of Technology, Hamirpur.'
+  ]
 
   constructor() { }
 
