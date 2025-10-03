4ce6bcb072406202d9a4741a3966ef364cbcd15d
Anukrit Tiwari <anukrit01@gmail.com>
2025-10-03 21:29:14 +0530

    updated code

--- my_portfolio/src/app/banner/banner.component.ts
+++ my_portfolio/src/app/banner/banner.component.ts
@@ -1,5 +1,5 @@
-import { Component, HostBinding, OnInit } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-banner',
